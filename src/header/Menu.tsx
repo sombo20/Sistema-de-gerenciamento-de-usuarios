f0a65ac8ff60811fcbbce3e4f7187653c8a30deb
Vicente Sombo <64757262+sombo20@users.noreply.github.com>
2022-12-06 15:28:43 +0100

    Update Menu.tsx

--- src/header/Menu.tsx
+++ src/header/Menu.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined,UserOutlined ,UserAddOutlined} from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Menu } from "antd";
+import { UserOutlined, UserAddOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const MenuItem = () => (
-  <Menu mode="horizontal" defaultSelectedKeys={['mail']}>
+  <Menu mode="horizontal" defaultSelectedKeys={["list"]}>
     <Menu.Item key="list" icon={<UserOutlined />}>
-    <Link to="/">LIst Users</Link>
+      <Link to="/">List Users</Link>
     </Menu.Item>
-    <Menu.Item key="register" icon={<UserAddOutlined />}>
-      <Link to="/register">Register new user</Link>
+    <Menu.Item key="list1" icon={<UserAddOutlined />}>
+      <Link to="/register">Register new User</Link>
     </Menu.Item>
   </Menu>
 );
