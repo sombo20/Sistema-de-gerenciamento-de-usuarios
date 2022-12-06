bf6739a37d275fd5888cea4c91813a5a647eba54
Vicente Sombo <vicentevictorsombo643@gmail.com>
2022-12-06 13:53:34 +0000

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,67 +1,40 @@
 import MenuItem from "../../header/Menu";
-import { List, Space } from "antd";
 import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  const [userdates, setUserdate] = useState();
   const token = import.meta.env.VITE_APP_TOKEN;
-  const request = async function (page: any) {
-    try {
-      const response = await fetch(
-        `https://gorest.co.in/public/v2/users?page=1&per_page=${page}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      const data = await response.json();
-    } catch (error) {}
-  };
+  const [userdates, setUserdate] = useState([]);
 
   useEffect(() => {
-    request(2);
-  }, []);
+    const request = async function () {
+      try {
+        const response = await fetch(
+          "https://gorest.co.in//public/v2/users?page=1&per_page=5",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
-  const data = Array.from({ length: 4 }).map((_, i) => ({
-    href: "https://ant.design",
-    title: `ant design part ${i}`,
-    description:
-      "Ant Design, a design language for background applications, is refined by Ant UED Team.",
-    content:
-      "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
-  }));
+        const data = await response.json();
+        setUserdate(data);
+      } catch (error) {}
+    };
+
+    request();
+  }, []);
 
   return (
     <>
       <MenuItem />
-      <List
-        itemLayout="vertical"
-        size="large"
-        pagination={{
-          onChange: (page) => {
-            console.log(page);
-          },
-          pageSize: 2,
-        }}
-        dataSource={data}
-        footer={
-          <div>
-            <b>ant design</b> footer part
-          </div>
+      {userdates.map((item: any) => {
+        {
+          item.id;
         }
-        renderItem={(item) => (
-          <List.Item key={item.title}>
-            <List.Item.Meta
-              title={<a href={item.href}>{item.title}</a>}
-              description={item.description}
-            />
-          </List.Item>
-        )}
-      />
+      })}
     </>
   );
 };
