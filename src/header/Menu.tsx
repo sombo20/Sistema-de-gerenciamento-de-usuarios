aa768d44e761e6d6ab57545f17e73ca290c7fc96
Vicente Sombo <vicentevictorsombo643@gmail.com>
2022-12-06 14:27:51 +0000

    Update Menu.tsx

--- src/header/Menu.tsx
+++ src/header/Menu.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const MenuItem = () => (
   <Menu mode="horizontal" defaultSelectedKeys={["list"]}>
     <Menu.Item key="list" icon={<UserOutlined />}>
-      <Link to="/">LIst Users</Link>
+      <Link to="/">List Users</Link>
     </Menu.Item>
     <Menu.Item key="list1" icon={<UserAddOutlined />}>
       <Link to="/register">Register new User</Link>
