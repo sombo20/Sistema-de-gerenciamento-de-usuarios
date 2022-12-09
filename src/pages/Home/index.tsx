e95d14a7d08e305a90896621b952ad72a8988444
Vicente Sombo <vicentevictorsombo643@gmail.com>
2022-12-09 17:50:32 +0000

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import MenuItem from "../../header/Menu";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { List, Space } from "antd";
 import {Link} from 'react-router-dom'
 
@@ -9,10 +9,13 @@ const Home = () => {
  const [Pages,setTotalPages] = useState<any>(null)
  let  page:string = Pages
  let Total  = +page;
+ const [loading,setLoading] = useState (false)
+ 
  
 
   const paginate = async function (page: any) {
     try {
+      setLoading(true)
       const response = await fetch(
         `https://gorest.co.in//public/v2/users?page=${page}&per_page=5`,
         {
@@ -30,19 +33,19 @@ const Home = () => {
       
       
       
-    } catch (error) {}
+    } catch (error) {}finally{
+      setLoading(false)
+    }
   };
 
-  useEffect(()=>{
-    paginate(1);
-  },[])
-
+  
   return (
     <>
       <MenuItem />
       <List
+        loading={loading}
         pagination={{
-          pageSize:5,
+          pageSize:10,
           total:Total,
           onChange:(page)=>{
             paginate(page)
