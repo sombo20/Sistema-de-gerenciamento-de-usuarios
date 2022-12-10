9b2964dae97a7a7ed18813bbe5d2f2f422561ed1
Vicente Sombo <64757262+sombo20@users.noreply.github.com>
2022-12-10 23:53:10 +0100

    Update index.tsx

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,18 +1,20 @@
 import MenuItem from "../../header/Menu";
-import { Button, notification, Checkbox, Form, Input, Radio } from "antd";
+import { notification } from "antd";
 import { Col, Row } from "antd";
 import { Typography } from "antd";
-import { UserOutlined, MailOutlined } from "@ant-design/icons";
 import { useState } from "react";
+import formUser from "../../components/formUser";
+import useForm from '../../components/Hook';
 
 const Register = () => {
-  const [form] = Form.useForm();
-  const [username, setUsername] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [gender, setGender] = useState<string>("");
-  const [status, setStatus] = useState<string>("");
   const token = import.meta.env.VITE_APP_TOKEN;
   const url = import.meta.env.VITE_APP_URL;
+  const [{
+    values
+  },
+    handleChange,
+    handleSubmit] = useForm();
+
 
   const [api, contextHolder] = notification.useNotification();
 
@@ -92,79 +94,9 @@ const Register = () => {
           <Title>Register new user</Title>
         </Col>
       </Row>
-
-      <Row>
-        <Col span={14} offset={9}>
-          <Form
-            name="basic"
-            form={form}
-            labelCol={{ span: 2 }}
-            wrapperCol={{ span: 10 }}
-            initialValues={{ remember: false }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-          >
-            <Form.Item
-              name="username"
-              rules={[
-                { required: true, message: "Please input your username!" },
-              ]}
-            >
-              <Input
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder="Username"
-              />
-            </Form.Item>
-
-            <Form.Item
-              name="email"
-              rules={[{ required: true, message: "Please input your email!" }]}
-            >
-              <Input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                prefix={<MailOutlined className="site-form-item-icon" />}
-                placeholder="Email"
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Gender"
-              name="gender"
-              rules={[
-                { required: true, message: "Please choose your gender!" },
-              ]}
-            >
-              <Radio.Group onChange={(e) => setGender(e.target.value)}>
-                <Radio value="male"> Male </Radio>
-                <Radio value="female"> Female </Radio>
-              </Radio.Group>
-            </Form.Item>
-
-            <Form.Item
-              label="state"
-              name="status"
-              rules={[
-                { required: true, message: "Please choose your status!" },
-              ]}
-            >
-              <Radio.Group onChange={(e) => setStatus(e.target.value)}>
-                <Radio value="active"> Active </Radio>
-                <Radio value="inactive"> Inactive </Radio>
-              </Radio.Group>
-            </Form.Item>
-
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                Register
-              </Button>
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
+      <formUser onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      valueChange={handleChange}
     </>
   );
 };
