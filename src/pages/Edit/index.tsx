335b4ed5196b5b48097b1affc9020259d1e4809b
Vicente Sombo <64757262+sombo20@users.noreply.github.com>
2022-12-11 18:33:05 +0100

    Update index.tsx

--- src/pages/Edit/index.tsx
+++ src/pages/Edit/index.tsx
@@ -1,37 +1,33 @@
 import MenuItem from "../../header/Menu";
-import { Button, notification, Form, Input, Radio } from "antd";
-import { Col, Row } from "antd";
-import { Typography } from "antd";
-import { UserOutlined, MailOutlined } from "@ant-design/icons";
-import { useEffect, useState } from "react";
+import { notification,Col, Row, Typography } from "antd";
+import { useEffect} from "react";
 import { useParams } from "react-router-dom";
+import FormUser from "../../components/Form";
+
 
 const EditUser = () => {
-  const [form] = Form.useForm();
-  const [username, setUsername] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [gender, setGender] = useState<string>("");
-  const [status, setStatus] = useState<string>("");
   const { id } = useParams();
   const [api, contextHolder] = notification.useNotification();
   const token = import.meta.env.VITE_APP_TOKEN;
   const url = import.meta.env.VITE_APP_URL;
-
-  const openNotification = () => {
+  const formState;
+  const openNotification = (message:string) => {
     api.open({
       message: "Edition",
-      description: "Success,User Edited",
+      description: message,
       duration: 0,
     });
   };
 
-  async function updateUser() {
-    try {
+  async function updateUser(userName:string,userEmail:string,userGender:string,userStatus:string,form){
+    
+  try {
+      formState = form
       const dates = JSON.stringify({
-        name: `${username}`,
-        email: `${email}`,
-        gender: `${gender}`,
-        status: `${status}`,
+        name:  userName,
+        email: userEmail,
+        gender: userGender,
+        status: userStatus,
       });
 
       const response = await fetch(`${url}${id}`, {
@@ -51,21 +47,11 @@ const EditUser = () => {
     }
   }
 
-  const onReset = () => {
-    form.resetFields();
-  };
-
-  const onFinish = () => {
-    updateUser();
-  };
-
-  const onFinishFailed = (errorInfo: any) => {
-    console.log("Failed:", errorInfo);
-  };
-
+  
   const { Title } = Typography;
 
   useEffect(() => {
+    Alert(formState)
     const request = async function () {
       try {
         const response = await fetch(`${url}${id}`, {
@@ -77,14 +63,14 @@ const EditUser = () => {
         });
 
         const data = await response.json();
-        form.setFieldsValue({
+        formState.setFieldsValue({
           username: data.name,
           email: data.email,
           status: data.status,
           gender: data.gender,
         });
       } catch (error) {
-        //alert("Request error ,please wait and try again")
+        openNotification("Please try again")
       }
     };
 
@@ -100,79 +86,7 @@ const EditUser = () => {
           <Title>Update User</Title>
         </Col>
       </Row>
-
-      <Row>
-        <Col span={14} offset={9}>
-          <Form
-            name="basic"
-            form={form}
-            labelCol={{ span: 2 }}
-            wrapperCol={{ span: 10 }}
-            initialValues={{ remember: false }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-          >
-            <Form.Item
-              name="username"
-              rules={[
-                { required: true, message: "Please input your username!" },
-              ]}
-            >
-              <Input
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder="Username"
-              />
-            </Form.Item>
-
-            <Form.Item
-              name="email"
-              rules={[{ required: true, message: "Please input your email!" }]}
-            >
-              <Input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                prefix={<MailOutlined className="site-form-item-icon" />}
-                placeholder="Email"
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Gender"
-              name="gender"
-              rules={[
-                { required: true, message: "Please choose your gender!" },
-              ]}
-            >
-              <Radio.Group onChange={(e) => setGender(e.target.value)}>
-                <Radio value="male"> Male </Radio>
-                <Radio value="female"> Female </Radio>
-              </Radio.Group>
-            </Form.Item>
-
-            <Form.Item
-              label="state"
-              name="status"
-              rules={[
-                { required: true, message: "Please choose your status!" },
-              ]}
-            >
-              <Radio.Group onChange={(e) => setStatus(e.target.value)}>
-                <Radio value="active"> Active </Radio>
-                <Radio value="inactive"> Inactive </Radio>
-              </Radio.Group>
-            </Form.Item>
-
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                Update
-              </Button>
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
+     <FormUser UserFunction={updateUser}/>
     </>
   );
 };
