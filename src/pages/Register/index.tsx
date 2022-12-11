576be0f95d46b9aa81e7e9f9b4c216de668d4e89
Vicente Sombo <64757262+sombo20@users.noreply.github.com>
2022-12-11 14:52:07 +0100

    Update index.tsx

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -90,7 +90,7 @@ const Register = () => {
           <Title>Register new user</Title>
         </Col>
       </Row>
-      <FormUser chama={hello()}/>
+      <FormUser chama={Hello()}/>
     </>
   );
 };
