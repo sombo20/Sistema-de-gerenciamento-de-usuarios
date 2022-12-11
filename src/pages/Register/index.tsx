8340964ea682036ce383e7033a3360e07d559b24
Vicente Sombo <64757262+sombo20@users.noreply.github.com>
2022-12-11 16:23:30 +0100

    Update index.tsx

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -51,13 +51,12 @@ const Register = () => {
       });
 
       const data = await response.json();
-      const message = data.message
-       if(message.indexOf("has already been taken") != -1){
-          // openNotification(message);
-alert()
-        }
-
-       // onReset();
+      const message = data.[0].message
+      alert(message)
+       /*if(message.indexOf("has already been taken") != -1){
+         openNotification(message);
+          alert()
+        }*/
       
     } catch (error) {
       alert(error)
