4e7cab04ec1d9346f9d91bdb0129eb29e53b112b
Vicente Sombo <64757262+sombo20@users.noreply.github.com>
2022-12-12 22:17:06 +0100

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -24,6 +24,8 @@ function FormUser({UserFunction}):USER{
     console.log("Failed:", errorInfo);
   };
 
+  form.setFieldsValue({username:"Domingos"});
+
   return (
       <Row>
         <Col span={14} offset={9}>
